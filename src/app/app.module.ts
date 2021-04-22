cc754d19e48d6c94e8413b2f64d951b2becea0de
serinamsri <namsri.sr@gmail.com>
2021-04-22 15:08:39 +0700

    add rating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UppercaseWithDotPipe } from './uppercase-with-dot.pipe';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UppercaseWithDotPipe
+    UppercaseWithDotPipe,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
